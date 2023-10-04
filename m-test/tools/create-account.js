884d7a3eb6cec7401b31770f64a38fa69a8fb73b
boaz <soonkuk@gmail.com>
2023-10-05 02:05:02 +0900

    feat: update create-account.js

--- m-test/tools/create-account.js
+++ m-test/tools/create-account.js
@@ -2,27 +2,13 @@ import axios from "axios";
 import wait from "waait";
 import { execSync } from "child_process";
 
-// import {
-// 	Amount,
-// 	Big,
-// 	Keys,
-// 	KPGen,
-// 	PubKey,
-// 	Currency,
-// 	TimeStamp,
-// 	Operation,
-// 	useId,
-// 	SIG_TYPE,
-// 	useSigType,
-// } from "mitum-sdk";
-
 import { Mitum } from "../../mitumjs/cjs/index.js";
-import { Big } from "../../mitumjs/cjs/utils/math.js";
-import { TimeStamp } from "../../mitumjs/cjs/utils/time.js";
-import { Amount } from "../../mitumjs/cjs/types/property.js";
-import { CreateAccountsItem, CreateAccountsFact } from "../../mitumjs/cjs/account/create.js";
-import { Keys, PubKey } from "../../mitumjs/cjs/account/publicKey.js"
-import { OperationType } from "../../mitumjs/cjs/types/operation.js"
+import { Big } from "../../mitumjs/cjs/types/math.js";
+import { TimeStamp } from "../../mitumjs/cjs/types/time.js";
+import { Amount } from "../../mitumjs/cjs/common/amount.js";
+import { CreateAccountItem, CreateAccountFact } from "../../mitumjs/cjs/operation/currency/create-account.js";
+import { Keys, PubKey } from "../../mitumjs/cjs/key/pub.js"
+import { Operation } from "../../mitumjs/cjs/operation/base/operation.js"
 
 import fs from "fs-extra";
 import { log, success, warning } from "../log.js";
@@ -56,142 +42,6 @@ async function run() {
 }
 
 await run();
-
-async function createAccount({
-	v,
-	mode,
-	network,
-	id,
-	cid,
-	maxItems,
-	genesis,
-	n,
-	interval,
-}) {
-	const token = new Date().getTime();
-	ensureDirSync(`logging/create-account-${token}/`);
-	log(`dir logging/create-account-${token}/ created`);
-
-	function totalPhase() {
-		let len = 0,
-			count = 0;
-		while (len < n) {
-			len += (len + 1) * maxItems;
-			count++;
-		}
-		return count;
-	}
-
-	const createAccounts = [];
-	let accounts = [
-		{
-			...genesis,
-		},
-	];
-
-	useId(id);
-	if (v === "v2") {
-		useSigType(SIG_TYPE.M2);
-	}
-
-	let phase = totalPhase() - 1;
-	let count = 0;
-
-    let base = 100000 * Math.pow(10, phase);
-	while (accounts.length <= n) {
-		log(`remain phase ${phase}`);
-		const accs = [];
-		for (const acc of accounts) {
-			const items = [];
-			for (let j = 0; j < maxItems; j++) {
-				const amounts = [
-					new Amount(
-						cid,
-						new Big(base * (phase * maxItems + 1)).toString()
-					),
-				];
-				const kp =
-					v === "v1"
-						? KPGen.random()
-						: v === "v2"
-						? KPGen.m2.random()
-						: null;
-				const keys = new Keys(
-					[new PubKey(kp.publicKey.toString(), 100)],
-					100
-				);
-				accs.push({
-					address: keys.address.toString(),
-					private: kp.privateKey.toString(),
-				});
-				items.push(new Currency.CreateAccountsItem(keys, amounts));
-				if (accounts.length + accs.length > n) {
-					break;
-				}
-			}
-
-			const fact = new Currency.CreateAccountsFact(
-				new TimeStamp().UTC(),
-				acc.address,
-				items
-			);
-			const op = new Operation(fact, "", []);
-			op.sign(acc.private);
-
-			const d = op.toHintedObject();
-			createAccounts.push(d);
-
-			writeFileSync(
-				`logging/create-account-${token}/${count}-${d.fact.hash}.json`,
-				JSON.stringify(d, null, 4)
-			);
-			log(
-				`logging/create-account-${token}/${count}-${d.fact.hash}.json created`
-			);
-
-			if (mode === "api") {
-				await axios
-					.post(`${network}/builder/send`, d)
-					.then((_) => success(`${count}:: OK; ${d.fact.hash}`))
-					.catch((_) => warning(`${count}:: BAD; ${d.fact.hash}`));
-				await wait(1000);
-			} else if (mode === "network-client") {
-				ensureDirSync(
-					`logging/create-account-${token}/network-client-log/`
-				);
-				log(
-					`dir logging/create-account-${token}/network-client-log/ created`
-				);
-				execSync(
-					`./m network client '{"_hint":"send-operation-header-v0.0.1"}' '${id}' ${network}#tls_insecure --body=logging/create-account-${token}/${count}-${d.fact.hash}.json --log.level=fatal > logging/create-account-${token}/network-client-log/${count}-${d.fact.hash}.json 2>&1`
-				);
-				success(`${count}:: ${d.fact.hash} sent by network-client`);
-				await wait(1000);
-			}
-			count++;
-			if (accounts.length + accs.length > n) {
-				break;
-			}
-		}
-		accounts = accounts.concat(...accs);
-		console.log("wait...");
-		await wait(interval);
-		phase--;
-        base = parseInt(base / 10);
-	}
-
-	writeFileSync(
-		`logging/create-account-${token}/accounts.json`,
-		JSON.stringify(
-			{
-				accounts: accounts.slice(1),
-			},
-			null,
-			4
-		)
-	);
-	log(`logging/create-account-${token}/accounts.json created`);
-}
 
 async function createAccountM2({
 	v,
@@ -230,10 +80,6 @@ async function createAccountM2({
 		console.error("v1 is unavailable with ../../mitumjs/");
 		exit(-1);
 	}
-	// useId(id);
-	// if (v === "v2") {
-	// 	useSigType(SIG_TYPE.M2);
-	// }
 
 	let phase = totalPhase() - 1;
 	let count = 0;
@@ -251,12 +97,7 @@ async function createAccountM2({
 						new Big(base * (phase * maxItems + 1)).toString()
 					),
 				];
-				// const kp =
-				// 	v === "v1"
-				// 		? KPGen.random()
-				// 		: v === "v2"
-				// 		? KPGen.m2.random()
-				// 		: null;
+
 				const kp = mitum.account.key();
 				const keys = new Keys(
 					[new PubKey(kp.publickey, 100)],
@@ -266,18 +107,18 @@ async function createAccountM2({
 					address: keys.address.toString(),
 					private: kp.privatekey,
 				});
-				items.push(new CreateAccountsItem(keys, amounts, "mitum"));
+				items.push(new CreateAccountItem(keys, amounts, "mitum"));
 				if (accounts.length + accs.length > n) {
 					break;
 				}
 			}
 
-			const fact = new CreateAccountsFact(
+			const fact = new CreateAccountFact(
 				new TimeStamp().UTC(),
 				acc.address,
 				items
 			);
-			const op = new OperationType(id, fact);
+			const op = new Operation(id, fact);
 			op.sign(acc.private);
 
 			const d = op.toHintedObject();
