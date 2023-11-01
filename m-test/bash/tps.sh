bf9b4b7c1a4a003b9f123bc2e7308c7273da1b57
soonkuk <soonkuk@gmail.com>
2023-11-01 11:13:01 +0900

    feat: update .gitignore

--- m-test/bash/tps.sh
+++
@@ -1,4 +0,0 @@
-TOKEN=transfer-1694085841304
-NETWORK=http://localhost:54320
-MONGO=""
-node tools/lookup.js $TOKEN $NETWORK $MONGO
--- m-test/bash/transfer.sh
+++
@@ -1,7 +0,0 @@
-ID=mitum
-CID=PEN
-TOTAL=20
-LOADS=1
-SENDERS=logging/create-account-1697295421008/accounts.json
-RECEIVERS=logging/create-account-1697295432522/accounts.json
-node tools/transfer.js $ID $CID $TOTAL $LOADS $SENDERS $RECEIVERS
