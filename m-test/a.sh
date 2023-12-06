609a01e8dfcf216389d09f1c36ba4813d83bca92
오하영 <alice0l02@naver.com>
2023-12-06 11:37:26 +0900

    feat: fix README.md

---
+++ m-test/a.sh
@@ -0,0 +1,7 @@
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000 
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000
+bash bash/create-operations.sh --data=1699935325742 --type=account --total=40000
